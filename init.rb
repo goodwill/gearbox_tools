34fe95b5c2776a04d7b1cbc4a0725f1413ee0350
William Yeung <william.yeung@gearboxsoft.com>
2008-10-29 13:31:19 +0800

    Added auto authenticated_system hookup

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
 # Include hook code here
 
 ActionController::Base.helper(ErrorMessageHelper)
+
+# by default we include authenticated system for all application controller, remark this to override
+ActionController::Base.send(:include, AuthenticatedSystem)
